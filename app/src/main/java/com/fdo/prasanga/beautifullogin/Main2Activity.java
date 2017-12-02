919e0272663ff585c0f81778788b9c8f027d8c28
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-12-02 13:24:11 +0530

    Final commit on the first development stage

---
+++ app/src/main/java/com/fdo/prasanga/beautifullogin/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.fdo.prasanga.beautifullogin;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
---
+++ app/src/main/java/com/fdo/prasanga/beautifullogin/MainActivity.java
@@ -0,0 +1,30 @@
+package com.fdo.prasanga.beautifullogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //@SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView cv_login;
+
+        cv_login = (TextView) findViewById(R.id.textView);
+
+        cv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
